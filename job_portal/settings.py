2e3da1784bd756adb632e805622640f93c91b24a
living-ghost <akhiiltkaniiparampiil@gmail.com>
2024-08-27 04:03:33 +0530

    NA

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -127,12 +127,12 @@ WSGI_APPLICATION = 'job_portal.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
+        'ENGINE': 'django_prometheus.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'dev_job_portal'),
+        'USER': os.getenv('DB_USER', 'dev'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
