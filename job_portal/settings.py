20f1d528827e2d21a32682bedd9c0f07df7e5014
living-ghost <akhiiltkaniiparampiil@gmail.com>
2024-08-29 01:59:20 +0530

    NA

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG')
 
 # Allowed hosts for the Django project
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
