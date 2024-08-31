e9cdf07693d680162c06b15f90f841726d86d857
living-ghost <akhiiltkaniiparampiil@gmail.com>
2024-09-01 03:03:45 +0530

    NA

--- portal_admin_app/tasks.py
+++ portal_admin_app/tasks.py
@@ -2,6 +2,7 @@ from celery import shared_task
 from django.core.mail import EmailMessage
 from django.conf import settings
 import logging
+from django.core.mail import send_mail
 
 logger = logging.getLogger(__name__)
 
@@ -9,25 +10,13 @@ logger = logging.getLogger(__name__)
 def send_job_email(subscriber_email, job_id, unsubscribe_url, site_url):
     logger.info(f"Sending email to {subscriber_email}")
     try:
-        subject = "New Job Posting"
-        message = f"""
-        Hi there,
-
-        A new job posting has been added. 
-
-        Job ID: {job_id}
-        Unsubscribe: {unsubscribe_url}
-        Visit our site: {site_url}
-        """
-        from_email = settings.DEFAULT_FROM_EMAIL
-        email = EmailMessage(
-            subject, 
-            message,
-            from_email,
-            print("before ", subscriber_email)
-            [subscriber_email]
+        send_mail(
+            'Your OTP Code',
+            f'Your OTP code is {job_id}.',
+            'akhiiltkaniiparampiil@gmail.com',
+            [subscriber_email],
+            fail_silently=False,
         )
-        email.send()
         logger.info("Email sent successfully")
     except Exception as e:
         logger.error(f"Error sending email: {e}")
