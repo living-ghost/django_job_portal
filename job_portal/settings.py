a4e04e807e362b615198cb624650d538495fdd74
living-ghost <akhiiltkaniiparampiil@gmail.com>
2024-09-01 20:20:58 +0530

    NA

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -134,9 +134,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # Email host user
+EMAIL_HOST_USER = 'akhiiltkaniiparampiil@gmail.com' or os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Email host password
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')  # Default from email address
+DEFAULT_FROM_EMAIL = 'akhiiltkaniiparampiil@gmail.com' or os.getenv('DEFAULT_FROM_EMAIL')
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
