f1e73bb309c0d33184a999225e50b232538b6d56
living-ghost <akhiiltkaniiparampiil@gmail.com>
2024-09-03 00:31:56 +0530

    email sending testing

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -14,6 +14,9 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,9 +137,13 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'akhiiltkaniiparampiil@gmail.com'
-EMAIL_HOST_PASSWORD = 'nair vsxs umsw hxur'
-DEFAULT_FROM_EMAIL = 'akhiiltkaniiparampiil@gmail.com'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
+print('EMAIL_HOST_USER:', os.getenv('EMAIL_HOST_USER'))
+print('EMAIL_HOST_PASSWORD:', os.getenv('EMAIL_HOST_PASSWORD'))
+print('DEFAULT_FROM_EMAIL:', os.getenv('DEFAULT_FROM_EMAIL'))
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
