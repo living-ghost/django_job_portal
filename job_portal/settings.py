34817e4e1f11510ef5daf1f91c0c7156438e9e9f
living-ghost <akhiiltkaniiparampiil@gmail.com>
2024-09-03 03:03:21 +0530

    email sending testing

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -136,12 +136,13 @@ DATABASES = {
 EMAIL_PWD_DEV = os.getenv('EMAIL_PWD')
 print(EMAIL_PWD_DEV)
 
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'akhiiltkaniiparampiil@gmail.com'
-EMAIL_HOST_PASSWORD = EMAIL_PWD_DEV
+EMAIL_HOST_PASSWORD = "'"+EMAIL_PWD_DEV+"'"
 print(EMAIL_HOST_PASSWORD)
 DEFAULT_FROM_EMAIL = 'akhiiltkaniiparampiil@gmail.com'
 
