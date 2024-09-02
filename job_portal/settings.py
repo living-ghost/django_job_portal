5e465be5d44baf0a55abef1e923a6d4f443f2289
living-ghost <akhiiltkaniiparampiil@gmail.com>
2024-09-03 02:59:49 +0530

    email sending testing

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -134,6 +134,7 @@ DATABASES = {
 
 # Email backend configuration
 EMAIL_PWD_DEV = os.getenv('EMAIL_PWD')
+print(EMAIL_PWD_DEV)
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
@@ -141,6 +142,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'akhiiltkaniiparampiil@gmail.com'
 EMAIL_HOST_PASSWORD = EMAIL_PWD_DEV
+print(EMAIL_HOST_PASSWORD)
 DEFAULT_FROM_EMAIL = 'akhiiltkaniiparampiil@gmail.com'
 
 # Password validation
