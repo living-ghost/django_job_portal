36b0c386e981fbcfdccd53aba8cb76dc334b0c46
living-ghost <akhiiltkaniiparampiil@gmail.com>
2024-09-20 23:14:58 +0530

    NA

--- portal_resume_app/views.py
+++ portal_resume_app/views.py
@@ -161,14 +161,13 @@ def resume_download_view(request, resume_id):
             'margin-right': '0mm',
             'margin-bottom': '0mm',
             'margin-left': '0mm',
-            'zoom': '1.0',
+            'zoom': '1.05',
             'print-media-type': '',
             'orientation': 'Portrait',
             'enable-local-file-access': '',
             'dpi': 300,
             'image-dpi': 300,
             'image-quality': 100,
-            'no-pdf-compression': '',
         }
 
         # Convert HTML to PDF with the specified options
