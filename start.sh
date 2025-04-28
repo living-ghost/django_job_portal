ea8764e6670b5ec9117dd9a3052aa172dec24b1a
living-ghost <akhiiltkaniiparampiil@gmail.com>
2025-04-28 23:37:44 +0530

    statifiles folder directy passing to git

--- start.sh
+++ start.sh
@@ -7,8 +7,5 @@ python manage.py makemigrations portal_converter_app
 python manage.py makemigrations portal_resume_app
 python manage.py migrate
 
-# Collect static files
-python manage.py collectstatic --noinput
-
 # Start Gunicorn server
 gunicorn --bind 0.0.0.0:8000 job_portal.wsgi:application
