22f43633d111c8f00816843be534fa101f7313af
living-ghost <akhiiltkaniiparampiil@gmail.com>
2025-05-01 23:30:11 +0530

    celery file updated to work with azure setups

--- job_portal/celery.py
+++ job_portal/celery.py
@@ -6,7 +6,7 @@ from kombu.exceptions import OperationalError
 app = Celery('job_portal')
 
 app.config_from_object({
-    'broker_url': 'amqp://guest:guest@freshersparkrabbitmq:5672//',
+    'broker_url': 'amqp://guest:guest@freshersparkrabbitmq.victoriousocean-8dff7969.centralindia.azurecontainerapps.io:5672//',
     'result_backend': 'rpc://',
     'task_default_retry_delay': 30,  # Retry every 30 seconds
     'task_max_retries': 10,  # Max retries before failing
